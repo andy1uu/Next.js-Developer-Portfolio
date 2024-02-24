fefc83b2cc7da9a7fd8db4b092fb92be1b944ffe
Andy Luu <andyluu324@gmail.com>
2024-02-23 21:32:10 -0500

    Did everything but make it reponsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,8 +8,11 @@ import {
   GithubIcon,
   LinkedInIcon,
   PinterestIcon,
+  SunIcon,
+  MoonIcon,
 } from "./Icons";
 import { motion } from "framer-motion";
+import useThemeSwitcher from "./hooks/useThemeSwitcher";
 
 const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
@@ -21,15 +24,17 @@ const CustomLink = ({ href, title, className = "" }) => {
       absolute left-0 -bottom-0.5 
       group-hover:w-full transition-[width] ease duration-300 ${
         router.asPath === href ? "w-full" : "w-0"
-      }`}
+      } dark:bg-light`}
       />
     </Link>
   );
 };
 
 const NavBar = () => {
+  const [mode, setMode] = useThemeSwitcher();
+
   return (
-    <header className="w-full px-16 py-8 font-medium flex items-center justify-between">
+    <header className="w-full px-16 py-8 font-medium flex items-center justify-between dark:text-light">
       <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
         <CustomLink href="/about" title="About" className="mx-4" />
@@ -83,6 +88,13 @@ const NavBar = () => {
           <DribbbleIcon />
         </motion.a>
       </nav>
+      <button onClick={() => setMode(mode === "light" ? "dark" : "light")} className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}>
+        {mode === "dark" ? (
+          <SunIcon className="fill-dark" />
+        ) : (
+          <MoonIcon className="fill-dark" />
+        )}
+      </button>
     </header>
   );
 };
